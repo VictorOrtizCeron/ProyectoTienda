168df379677043e6245b3f7351c38a008ef9299e
Victor Ortiz <victorandresortiz@gmail.com>
2024-12-16 19:25:06 -0600

    Agregación de roles y codigo para carrito clase semana 13 !

--- src/main/java/com/tienda/Tienda/service/impl/ItemServiceImpl.java
+++ src/main/java/com/tienda/Tienda/service/impl/ItemServiceImpl.java
@@ -1,9 +1,18 @@
 package com.tienda.Tienda.service.impl;
 
+import com.tienda.Tienda.dao.FacturaDao;
+import com.tienda.Tienda.dao.ProductoDao;
+import com.tienda.Tienda.dao.UsuarioDao;
+import com.tienda.Tienda.dao.VentaDao;
+import com.tienda.Tienda.domain.Factura;
 import com.tienda.Tienda.domain.Item;
+import com.tienda.Tienda.domain.Usuario;
+import com.tienda.Tienda.domain.Venta;
 import com.tienda.Tienda.service.ItemService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -94,5 +103,87 @@ public class ItemServiceImpl implements ItemService {
                 }
             }
         }
+    }
+    @Autowired
+    private UsuarioDao usuarioDao;
+    @Autowired
+    private ProductoDao productoDao;
+    @Autowired
+    private FacturaDao facturaDao;
+    @Autowired
+    private VentaDao ventaDao;
+
+    @Override
+    public void facturar() {
+        //Se debe recuperar el usuario autenticado y obtener su idUsuario
+        String username = "";
+        var principal = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        if (principal instanceof UserDetails userDetails) {
+            username = userDetails.getUsername();
+        } else {
+            if (principal != null) {
+                username = principal.toString();
+            }
+        }
+
+        if (username.isBlank()) {
+            System.out.println("username en blanco...");
+            return;
+        }
+
+        Usuario usuario = usuarioDao.findByUsername(username);
+        if (usuario == null) {
+            System.out.println("Usuario no existe en usuarios...");
+            return;
+        }
+
+        //Se debe registrar la factura incluyendo el usuario
+        Factura factura = new Factura(usuario.getIdUsuario());
+        factura = facturaDao.save(factura);
+
+        //Se debe registrar las ventas de cada producto -actualizando existencias-
+        @SuppressWarnings("unchecked")
+        List<Item> listaItems = (List) session.getAttribute("listaItems");
+        if (listaItems != null) {
+            double total = 0;
+            for (Item i : listaItems) {
+                var producto = productoDao.getReferenceById(i.getIdProducto());
+                if (producto.getExistencias() >= i.getCantidad()) {
+                    Venta venta = new Venta(factura.getIdFactura(),
+                            i.getIdProducto(),
+                            i.getPrecio(),
+                            i.getCantidad());
+                    ventaDao.save(venta);
+                    producto.setExistencias(producto.getExistencias() - i.getCantidad());
+                    productoDao.save(producto);
+                    total += i.getCantidad()* i.getPrecio();
+                }
+            }
+
+            //Se debe registrar el total de la venta en la factura
+            factura.setTotal(total);
+            facturaDao.save(factura);
+
+            //Se debe limpiar el carrito la lista...
+            listaItems.clear();
+        }
+    }
+
+    @Override
+    public double getTotal() {
+        //Se debe registrar las ventas de cada producto -actualizando existencias-
+        @SuppressWarnings("unchecked")
+        var listaItems = (List<Item>) session.getAttribute("listaItems");
+        double total = 0;
+        if (listaItems != null) {
+            for (Item i : listaItems) {
+                total += i.getCantidad()* i.getPrecio();
+            }
+        }
+        return total;
     }
 }
